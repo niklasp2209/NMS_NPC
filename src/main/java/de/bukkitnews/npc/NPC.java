ee3df3399b92ccaa9a0028f4d09c821943894f8d
Niklas Petermeier <niklasp2209@outlook.de>
2023-01-08 00:12:38 +0100

    created project

---
+++ src/main/java/de/bukkitnews/npc/NPC.java
@@ -0,0 +1,16 @@
+package de.bukkitnews.npc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NPC extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
